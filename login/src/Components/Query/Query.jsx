d20823858febcf31abf6a2d75b4887ce5de6b3dc
bharani2624 <bharani2624baru@gmail.com>
2024-09-22 17:53:03 +0530

    Added Query For All Issues

--- login/src/Components/Query/Query.jsx
+++ login/src/Components/Query/Query.jsx
@@ -1,25 +1,51 @@
 import React from "react";
 import Popup from 'reactjs-popup';
-const Query=()=>
-    {
-return(   
-         <div className="query h-screen w-screen flex items-center space-x-2">
-           <Popup trigger={
+const Query = () => {
+    return (
+        <div className="query h-screen w-screen flex items-center space-x-2">
+            <Popup trigger={
                 <div className="food cursor-pointer bg-white w-full h-2/6 text-black font-bold text-4xl border rounded-lg flex items-center justify-center hover:bg-slate-200">
                     Food
                 </div>
             } position="top" modal>
                 <div className="p-4 text-center text-lg font-semibold bg-white w-full h-full outline-none border rounded-lg shadow-xl">
-                <textarea className="text-lg h-44 outline-none" type="text" placeholder="Enter Your Issue"/>
-                <div className="submit bg-white cursor-pointer w-full">Submit</div>
+                    <textarea className="text-lg h-44 outline-none" type="text" placeholder="Enter Your Issue" />
+                    <div className="submit bg-white cursor-pointer w-full">Submit</div>
                 </div>
             </Popup>
-             <div className="room cursor-pointer bg-white w-full h-2/6 text-black font-bold text-4xl border rounded-lg flex items-center justify-center hover:bg-slate-200">Room</div>
-             <div className="hostel cursor-pointer bg-white w-full h-2/6 text-black font-bold text-4xl border rounded-lg flex items-center justify-center hover:bg-slate-200">Hostel</div>
-             <div className="toilets cursor-pointer bg-white w-full h-2/6 text-black font-bold text-4xl border rounded-lg flex items-center justify-center hover:bg-slate-200">Toilets</div>
-             <div className="others cursor-pointer bg-white w-full h-2/6 text-black font-bold text-4xl border rounded-lg flex items-center justify-center hover:bg-slate-200">Others..</div>
-
+            <Popup trigger={
+                <div className="room cursor-pointer bg-white w-full h-2/6 text-black font-bold text-4xl border rounded-lg flex items-center justify-center hover:bg-slate-200">
+                    Room
+                </div>
+            } position="top" modal>
+                <div className="p-4 text-center text-lg font-semibold bg-white w-full h-full outline-none border rounded-lg shadow-xl">
+                    <textarea className="text-lg h-44 outline-none" type="text" placeholder="Enter Your Issue" />
+                    <div className="submit bg-white cursor-pointer w-full">Submit</div>
+                </div>
+            </Popup>
+            <Popup trigger={
+                <div className="hostel cursor-pointer bg-white w-full h-2/6 text-black font-bold text-4xl border rounded-lg flex items-center justify-center hover:bg-slate-200">Hostel</div>
+            } position="top" modal>
+                <div className="p-4 text-center text-lg font-semibold bg-white w-full h-full outline-none border rounded-lg shadow-xl">
+                    <textarea className="text-lg h-44 outline-none" type="text" placeholder="Enter Your Issue" />
+                    <div className="submit bg-white cursor-pointer w-full">Submit</div>
+                </div>
+            </Popup>
+            <Popup trigger={
+                <div className="toilets cursor-pointer bg-white w-full h-2/6 text-black font-bold text-4xl border rounded-lg flex items-center justify-center hover:bg-slate-200">Toilets</div>
+            } position="top" modal>
+                <div className="p-4 text-center text-lg font-semibold bg-white w-full h-full outline-none border rounded-lg shadow-xl">
+                    <textarea className="text-lg h-44 outline-none" type="text" placeholder="Enter Your Issue" />
+                    <div className="submit bg-white cursor-pointer w-full">Submit</div>
+                </div>
+            </Popup >
+            <Popup trigger={
+                <div className="others cursor-pointer bg-white w-full h-2/6 text-black font-bold text-4xl border rounded-lg flex items-center justify-center hover:bg-slate-200">Others..</div>
+            } position="top" modal> <div className="p-4 text-center text-lg font-semibold bg-white w-full h-full outline-none border rounded-lg shadow-xl">
+                    <textarea className="text-lg h-44 outline-none" type="text" placeholder="Enter Your Issue" />
+                    <div className="submit bg-white cursor-pointer w-full">Submit</div>
+                </div></Popup>
         </div>
-        )
-    }
+    )
+}
 export default Query;
